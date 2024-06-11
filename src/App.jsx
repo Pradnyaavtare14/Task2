475dc225fb70002c35ebca26618e72d8300db7ff
pradnya <avtarepradnya@gmail.com>
2024-06-11 22:12:49 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 // import React from 'react'
 
-import Features from "./components/Features"
-import Footer from "./components/Footer"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import NextPage from "./components/NextPage"
+import Login11 from "./components/Login11"
+import Login12 from "./components/Login12"
+import Login13 from "./components/Login13"
+import Login14 from "./components/Login14"
+import Login15 from "./components/Login15"
+import Login16 from "./components/Login16"
+import Login17 from "./components/Login17"
+import Login18 from "./components/Login18"
+import Login19 from "./components/Login19"
+import Login20 from "./components/Login20"
+
 
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <NextPage/>
-      <Features/>
-      <Footer/>
+    <Login11/>
+    <Login12/>
+    <Login13/>
+    <Login14/>
+    <Login15/>
+    <Login16/>
+    <Login17/>
+    <Login18/>
+    <Login19/>
+    <Login20/>
+
     </div>
   )
 }
